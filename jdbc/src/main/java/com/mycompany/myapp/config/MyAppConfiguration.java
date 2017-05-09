61c56ae0178f07026db314a6f265b2d1701c4039
aman <amanht3@gmail.com>
2017-05-09 13:07:02 +0545

    first commit

---
+++ jdbc/src/main/java/com/mycompany/myapp/config/MyAppConfiguration.java
@@ -0,0 +1,24 @@
+package com.mycompany.myapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by aman on 5/9/17.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mycompany.myapp")
+public class MyAppConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
